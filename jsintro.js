dff9e28bc33f5c8e1dcd2d2a8c641eea1295371f
kanakgupta01-debug <kanakgupta01@icloud.com>
2026-02-24 21:32:14 +0530

    javascript

---
+++ jsintro.js
@@ -0,0 +1,11 @@
+//cross-platform, object-based cripting language for client and server application
+//loosely typed language
+//interpreted at just-in-time compilation(runtime)
+//high-level and multipardigm
+//can be embedded with html
+//FEATURES:extends functionality of web page
+//written in html document
+//controls page elements
+//object oriented
+//speed,simplicity
+//js and java are not similar
